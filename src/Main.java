fabe8f6d66646d739dc18f6d964e8ee725c6768f
ArielSanchez12 <riveraariel433@gmail.com>
2024-11-07 09:55:56 -0500

    PracticaTryCatchException

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,14 @@ public class Main {
         posicion = scanner.nextInt();
 
         try {
-            System.out.println("La edad en la posicion " + posicion + " es: " + edades[posicion]);
+            System.out.println("La edad en la posicion " + posicion + " es: " + edades[posicion]); //Intentaras (TRY) acceder a una posicion dentro del array, segun esto, se dgenerara una excepcion en caso de que el TRY falle
+                                                                                                    //En caso de que no falle, pues se va a ejecutar lo que esta aqui adentro
         }
-        catch (Exception error) {
-            System.out.println("Intentaste ingresar a una posicion que no existe");
+        catch (Exception error) { //Atrapa el error que se genero arriba
+            System.out.println("Intentaste ingresar a una posicion que no existe"); //Y hace lo que le pongamos aqui
+            //El proposito del catch es que el programa se siga ejecutando una vez encuentre un error, ya que sin esto el programa se va a detener
         }
 
-        System.out.println("Richard Peruano");
+        System.out.println("Richard Peruano"); //Esta es una linea para probar si se sigue ejecutando el programa
     }
 }
